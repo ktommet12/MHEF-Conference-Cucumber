6a11c86bb44aa89bb2db82e1a78cffe7b3cce6c5
Kyle Tommet <ktommet@gmail.com>
2017-11-05 14:10:52 -0700

    initial commit

---
+++ features/pages/GooglePage.rb
@@ -0,0 +1,5 @@
+class GooglePage
+    include PageObject
+
+    url "https://google.com"
+end
---
+++ features/services/RestService.rb
@@ -0,0 +1,13 @@
+require 'rest-client'
+require 'json'
+class RestService
+    def initialize
+        @current_conference = ENV['conf']
+    end
+
+
+    def get_schedule_json
+    
+    end
+
+end
